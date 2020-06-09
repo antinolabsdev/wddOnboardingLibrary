e16ecbce5238ce7b7992985723b87c5af8af1e9f
harishantino <59561098+harishantino@users.noreply.github.com>
2020-06-09 14:02:25 +0530

    changes in sample app

--- app/src/main/java/com/library/wdd/MainActivity.java
+++ app/src/main/java/com/library/wdd/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final CustomButton relativeLayout = new CustomButton(MainActivity.this);
-        CustomButton.KeyAccess("AKIAZID35LK5ROAGCPHJ","wimdF3AVdcNLGsVzUcBv363/yjXjQ5Mr1fCX0nIm");
+        CustomButton.KeyAccess("","");
         final ConstraintLayout constraintLayout =  findViewById(R.id.parent);
         final TextView textView = findViewById(R.id.tvClick);
 
