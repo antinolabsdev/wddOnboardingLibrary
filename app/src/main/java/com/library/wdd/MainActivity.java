ed4a2032145b7b49304b8607d4c9f38df4c2938b
harishantino <59561098+harishantino@users.noreply.github.com>
2020-06-17 18:49:57 +0530

    remove all the keys

--- app/src/main/java/com/library/wdd/MainActivity.java
+++ app/src/main/java/com/library/wdd/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final CustomButton relativeLayout = new CustomButton(MainActivity.this);
 
-        CustomButton.KeyAccess("AKIA2FUYI7S2MOWWDL4W","mTaaNUa8t7InNVNNYq+gptb4QdLmHcFE4/HyDcrI","us-east-1:153fc86c-6b7c-4a1b-b253-9948f9409c4d","wddonboardingbucket");
+        CustomButton.KeyAccess("","","","");
         final ConstraintLayout constraintLayout =  findViewById(R.id.parent);
         final TextView textView = findViewById(R.id.tvClick);
 
